3f579c02a7b816d9a8bffa5044e70cd4bac61e8a
Kepi <kepi@igloonet.cz>
2024-07-24 19:52:46 +0200

    support reading license key from env variable MAXMIND_LICENSE

--- generate.sh
+++ generate.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 BASE_DIR=$(dirname $0)
-YOUR_LICENSE_KEY=""
 
 GEOIP_COUNTRIES_ZIP="${BASE_DIR}/output/geoip_countries.zip"
 GEOIP_COUNTRIES="${BASE_DIR}/output/geoip_countries"
@@ -22,12 +21,12 @@ while [[ $# -gt 0 ]]; do
             shift
             ;;
         --license)        # Your MaxMind license key
-            YOUR_LICENSE_KEY="$2"
+            MAXMIND_LICENSE="$2"
             shift
             shift
             ;;
         *)   # Call a "show_help" function to display a synopsis, then exit.
-            echo "./generate.sh --license YOUR_LICENSE_KEY --out /etc/haproxy/geoip2"
+            echo "./generate.sh --license MAXMIND_LICENSE --out /etc/haproxy/geoip2"
             echo ""
             echo "--out          Output directory for subnets"
             echo "--license      A MaxMind.com license key. Get it from maxmind.com -> My Account -> My License Key"
@@ -38,7 +37,7 @@ done
 
 mkdir -p "output" "$COUNTRIES" "$CONTINENTS"
 
-if [ -z "$YOUR_LICENSE_KEY" ]; then
+if [ -z "$MAXMIND_LICENSE" ]; then
     echo "MaxMind license key must be set via --liecense parameter. See --help for more.";
     exit 1;
 fi
@@ -52,7 +51,7 @@ if ! find "$GEOIP_COUNTRIES_ZIP" -mtime -7 2>/dev/null; then
     echo "Downloading https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country-CSV&license_key=xxxxx&suffix=zipGeoLite2-Country-CSV..."
 
     # abort the script if the download failed
-    if ! wget -q -O "$GEOIP_COUNTRIES_ZIP" "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country-CSV&license_key=$YOUR_LICENSE_KEY&suffix=zip"; then
+    if ! wget -q -O "$GEOIP_COUNTRIES_ZIP" "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country-CSV&license_key=$MAXMIND_LICENSE&suffix=zip"; then
         echo "Error downloading file"
         exit 1
     fi
